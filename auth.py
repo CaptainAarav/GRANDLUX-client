e29b14d5a99c164db88966b9a9a277e9d79c0376
CaptainAarav <aaravsahni1037@gmail.com>
2026-07-30 07:50:06 +0530

    Added loginlistener for external page login

---
+++ auth.py
@@ -0,0 +1,53 @@
+import webbrowser
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+
+CALLBACK_PORT = 8734
+LOGIN_URL = "https://grandlux.lu/login"
+
+class CallbackHandler(BaseHTTPRequestHandler):
+    received_token = None
+
+    def do_GET(self):
+        query = parse_qs(urlparse(self.path).query)
+        CallbackHandler.received_token = query.get("token", [None])[0]
+
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>You're logged in - you can close this tab.</h1>")
+
+    def log_message(self, format, *args):
+        pass
+
+class LoginListener:
+    def __init__(self, port):
+        self.port = port
+        self.token = None
+        self._lock = threading.Lock()
+        self._running = False
+        
+    def start(self) -> None:
+        self._running = True
+        thread = threading.Thread(target=self._wait_for_callback, daemon=True)
+        thread.start()
+        
+    def stop(self) -> None:
+        self._running = False
+    
+    def _wait_for_callback(self) -> None:
+        server = HTTPServer(("127.0.0.1", self.port), CallbackHandler)
+        server.timeout = 0.5
+        
+        while self._running:
+            server.handle_request()
+            if CallbackHandler.received_token is not None:
+                break
+            
+        with self._lock:
+            self.token = CallbackHandler.received_token
+            
+    def get_token(self):
+        with self._lock:
+            return self.token
